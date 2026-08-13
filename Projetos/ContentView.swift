c1640a9526a5963a15fb614de0b19d8ea2bd4586
João Duque Nardelli Wandermuren <joao.dnw@icloud.com>
2026-08-13 16:02:05 -0300

    Initial Commit

---
+++ Projetos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projetos
+//
+//  Created by João Duque Nardelli Wandermuren on 13/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projetos/ProjetosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetosApp.swift
+//  Projetos
+//
+//  Created by João Duque Nardelli Wandermuren on 13/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjetosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
